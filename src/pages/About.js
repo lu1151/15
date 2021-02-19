a8319cb297a7bb507c12e61249f907a836c21fd1
lu1151 <luxi.li@outlook.com>
2021-02-19 00:11:29 -0500

    init commit

---
+++ src/pages/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about me</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti praesentium dolore nihil voluptas suscipit exercitationem saepe. Cum, impedit. Reiciendis, nisi enim, et libero quibusdam minima, laboriosam totam maxime molestiae consequatur dolorum optio perspiciatis nam aut facilis est repellat eos recusandae!
+      </p>
+    </section>
+  )
+}
+
+export default About
